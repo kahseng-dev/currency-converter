93aae9f7abe36046e1ea353f98521dd21acd88f6
kahseng-dev <kahseng.chew@protonmail.com>
2025-05-02 01:10:30 +0800

    feat: add convert page base

---
+++ app/(tabs)/convert.tsx
@@ -0,0 +1,30 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function ConvertScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Convert Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 12,
+    textDecorationLine: 'underline',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 12,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
