4f984fe2c568e956456afc27654e5e11fd9e4187
kahseng-dev <kahseng.chew@protonmail.com>
2025-05-02 08:25:05 +0800

    feat: add convert page base

--- app/(tabs)/convert.tsx
+++ app/(tabs)/convert.tsx
@@ -1,30 +1,65 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, Pressable, StyleSheet } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useState } from 'react';
 
 export default function ConvertScreen() {
+
+  const [amount, setAmount] = useState(1000);
+  const [fromCurrency, setFromCurrency] = useState('GBP');
+  const [toCurrency, setToCurrency] = useState('USD');
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Convert Screen</Text>
+    <View style={styles.view}>
+      <Pressable 
+        style={styles.buttonContainer}
+        onPress={() => alert('You pressed a button.')}>
+          <View style={styles.buttonText}>
+            <Text style={styles.text}>🇬🇧 {fromCurrency}</Text>
+            <Ionicons name="chevron-down" size={14} color="#fff" />
+          </View>
+          <Text style={styles.text}>{amount}</Text>
+      </Pressable>
+      <Pressable 
+        style={styles.buttonContainer}
+        onPress={() => alert('You pressed a button.')}>
+          <View style={styles.buttonText}>
+            <Text style={styles.text}>🇬🇧 {toCurrency}</Text>
+            <Ionicons name="chevron-down" size={14} color="#fff" />
+          </View>
+          <Text style={styles.text}>1,000</Text>
+      </Pressable>
+      <Text style={styles.span}>1 {fromCurrency} = 1 {toCurrency} at mid-market rate</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  view: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    fontSize: 12,
-    textDecorationLine: 'underline',
+    backgroundColor: '#323233',
+    padding: 24,
+    gap: 24,
   },
   text: {
+    fontSize: 24,
     color: '#fff',
   },
-  button: {
-    fontSize: 12,
-    textDecorationLine: 'underline',
+  span: {
+    fontSize: 14, 
+    textAlign: 'center',
     color: '#fff',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 24,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#fff',
+  },
+  buttonText: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    gap: 8
   },
 });
