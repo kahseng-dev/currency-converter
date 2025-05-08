3a2ac2f6054a366bebdf7634e2e7318506f60820
kahseng-dev <kahseng.chew@protonmail.com>
2025-05-08 15:05:06 +0800

    feat: add layout

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,17 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, Pressable, StyleSheet } from "react-native";
 
 export default function SettingsScreen() {
+  const appVersion = 0.1;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Settings Screen</Text>
+      <Pressable style={styles.button} 
+        onPress={() => alert('You pressed a button.')}>
+        <Text style={styles.buttonLabel}>Auto</Text>
+      </Pressable>
+      <Text style={styles.text}>General settings</Text>
+      <Text style={styles.text}>Blank</Text>
+      <Text style={styles.text}>Version {appVersion}</Text>
     </View>
   );
 }
@@ -22,9 +30,24 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
   },
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
   button: {
-    fontSize: 12,
-    textDecorationLine: 'underline',
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonLabel: {
     color: '#fff',
+    fontSize: 16,
   },
 });
