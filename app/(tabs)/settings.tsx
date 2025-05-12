135fb6196c04e09d4cfb46d9b58e5a668486599d
kahseng-dev <kahseng.chew@protonmail.com>
2025-05-12 21:27:18 +0800

    style: update settings screen

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,53 +1,63 @@
 import { Text, View, Pressable, StyleSheet } from "react-native";
+import { Colors } from '@/constants/Colors';
+
+const APP_VERSION:string = "0.1.0";
 
 export default function SettingsScreen() {
-  const appVersion = 0.1;
-
   return (
-    <View style={styles.container}>
-      <Pressable style={styles.button} 
-        onPress={() => alert('You pressed a button.')}>
-        <Text style={styles.buttonLabel}>Auto</Text>
-      </Pressable>
-      <Text style={styles.text}>General settings</Text>
-      <Text style={styles.text}>Blank</Text>
-      <Text style={styles.text}>Version {appVersion}</Text>
+    <View style={styles.main}>
+      <Text style={[styles.header, styles.text]}>Dark mode</Text>
+      <View>
+        <Pressable style={styles.themeSelectContainer} 
+          onPress={() => alert('You pressed a button.')}>
+          <Text style={[styles.themeOption, styles.themeOptionSelected]}>Auto</Text>
+          <Text style={styles.themeOption}>On</Text>
+          <Text style={styles.themeOption}>Off</Text>
+        </Pressable>
+      </View>
+      <Text style={[styles.header, styles.text]}>General settings</Text>
+      <Text style={styles.text}>Version {APP_VERSION}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    fontSize: 12,
-    textDecorationLine: 'underline',
+    backgroundColor: Colors.light.background,
+    padding: 24,
+    gap: 24,
   },
   text: {
-    color: '#fff',
+    fontSize: 14,
+    color: Colors.light.text,
   },
-  buttonContainer: {
-    width: 320,
-    height: 68,
-    marginHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 3,
+  header: {
+    paddingBottom: 8,
+    borderBottomWidth: 2,
+    borderBottomColor: Colors.light.icon,
   },
-  button: {
-    borderRadius: 10,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
+  themeSelectContainer: {
     flexDirection: 'row',
+    borderRadius: 8,
+    padding: 4,
+    gap: 12,
+    backgroundColor: Colors.light.cardBackground,
+  },
+  themeOption: {
+    flex: 1,
+    textAlign: 'center',
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    borderRadius: 8,
+  },
+  themeOptionSelected: {
+    fontWeight: 'bold',
+    color: Colors.light.tint,
+    backgroundColor: Colors.light.background,
   },
   buttonLabel: {
-    color: '#fff',
-    fontSize: 16,
+    color: Colors.light.text,
+    fontSize: 24,
   },
 });
