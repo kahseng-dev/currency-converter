6e3e634f51f2427db0df4453300c3593e207b661
kahseng-dev <kahseng.chew@protonmail.com>
2025-06-09 02:42:28 +0800

    feat: update button style

--- constants/styles.ts
+++ constants/styles.ts
@@ -2,5 +2,5 @@ export const styles = {
     font_mono: { fontFamily: 'Geist Mono' },
     icon: 18,
     icon_flag_height: 24,
-    button: 'rounded border py-2 px-4 duration-300 transition hover:bg-black/20',
+    button: 'py-2 px-4 rounded border duration-300 transition hover:bg-black/20',
 }
