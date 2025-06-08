70302402909992c3507ac367ec89174d6d5d6cfe
kahseng-dev <kahseng.chew@protonmail.com>
2025-06-08 16:22:22 +0800

    feat: add navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,51 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Tabs } from 'expo-router';
+
+import { styles } from '@/constants/styles';
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen 
+        name='index'
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ focused }) => (
+            <Ionicons 
+              name={focused ? 'home' : 'home-outline'}
+              className={styles.icon} />
+          ),
+        }} />
+      <Tabs.Screen
+        name='convert' 
+        options={{
+          title: 'Convert',
+          tabBarIcon: ({ focused }) => (
+            <Ionicons 
+              name={focused ? 'swap-horizontal' : 'swap-horizontal-outline'} 
+              className={styles.icon} />
+          ),
+        }} />
+      <Tabs.Screen
+        name='widgets' 
+        options={{
+          title: 'Widgets',
+          tabBarIcon: ({ focused }) => (
+            <Ionicons 
+              name={focused ? 'grid' : 'grid-outline'} 
+              className={styles.icon} />
+          ),
+        }} />
+      <Tabs.Screen
+        name='settings' 
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ focused }) => (
+            <Ionicons 
+              name={focused ? 'settings' : 'settings-outline'} 
+              className={styles.icon} />
+          ),
+        }} />
+    </Tabs>
+  );
+}
