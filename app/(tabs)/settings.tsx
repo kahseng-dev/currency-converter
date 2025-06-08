adfb2d1cf2fb58c2497e2bf524bfe395259507a5
kahseng-dev <kahseng.chew@protonmail.com>
2025-06-08 15:48:30 +0800

    feat: add settings page

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Settings() {
+  return (
+    <View className='flex items-center justify-center bg-black'>
+      <Text className='text-center'>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
