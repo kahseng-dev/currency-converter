95e6bac2630924b97cae83aa0226618e9336dc78
kahseng-dev <kahseng.chew@protonmail.com>
2025-06-12 17:14:05 +0800

    feat: add search function

--- app/add-favourites.tsx
+++ app/add-favourites.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Pressable, ScrollView, Text, View } from 'react-native';
+import { FlatList, Pressable, ScrollView, Text, TextInput, View } from 'react-native';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
 
@@ -10,50 +10,54 @@ export default function AddFavourites() {
   const [from, setFrom] = useState('');
   const [to, setTo] = useState('');
 
-  const [currencies, setCurrencies] = useState({
+  const currencies = {
     suggested: ['USD','GBP', 'SGD', 'EUR', 'INR'],
     all: ['AED','ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CHF', 'CHF', 'CLP', 'CNH', 'CNY', 'COP', 'CRC', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ETB', 'EUR', 'FJD', 'FKP', 'GBP', 'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HTG', 'HUF', 'IDR', 'ILS', 'IMP', 'INR', 'ISK', 'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KMF', 'KRW', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MUR', 'MVR', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SEK', 'SGD', 'SHP', 'SLE', 'SLL', 'SRD', 'STD', 'SVC', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD', 'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'UYU', 'UZS', 'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XCG', 'XOF', 'XPF', 'ZAR', 'ZWG'],
-  });
+  };
+
+  const [showSearchInput, setShowSearchInput] = useState(false);
+  const [searchInput, setSearchInput] = useState('');
+  const [searchResults, setSearchResults] = useState([...currencies.all]);
 
   const currencyName = new Intl.DisplayNames(['en'], { type: 'currency' });
 
   const loadSuggestedCurrencies = () => {
     return (
-      <View className='flex gap-4'>
-        {currencies.suggested.map(currency => 
+      <FlatList
+        data={currencies.suggested}
+        renderItem={currency => 
           <Pressable 
-            onPress={() => handleCurrencyPress(currency)}
-            key={currency}
-            className='flex flex-row gap-4'>
-            <Text style={styles.font_mono}>{currency}</Text>
+            onPress={() => handleCurrencyPress(currency.item)}
+            key={currency.index}
+            className='p-2 flex flex-row gap-4 hover:bg-neutral-300'>
+            <Text style={styles.font_mono}>{currency.item}</Text>
             <Text 
               className='text-neutral-500'
               style={styles.font_mono}>
-              {currencyName.of(currency)}
+              {currencyName.of(currency.item)}
             </Text>
           </Pressable>
-        )}
-      </View>
+        }/>
     )
   }
 
   const loadAllCurrencies = () => {
     return (
-      <View className='flex gap-4'>
-        {currencies.all.map(currency => 
+      <FlatList 
+        data={currencies.all}
+        renderItem={currency => 
           <Pressable 
-            onPress={() => handleCurrencyPress(currency)}
-            key={currency}
-            className='flex flex-row gap-4'>
-            <Text style={styles.font_mono}>{currency}</Text>
+            onPress={() => handleCurrencyPress(currency.item)}
+            key={currency.index}
+            className='p-2 flex flex-row gap-4 hover:bg-neutral-300'>
+            <Text style={styles.font_mono}>{currency.item}</Text>
             <Text 
               className='text-neutral-500'
               style={styles.font_mono}>
-              {currencyName.of(currency)}
+              {currencyName.of(currency.item)}
             </Text>
           </Pressable>
-        )}
-      </View>
+        }/>
     )
   }
 
@@ -66,7 +70,7 @@ export default function AddFavourites() {
       return setTo(currency)
     }
 
-    return 
+    return
   }
 
   const handleSwapFieldsPress = () => {
@@ -76,11 +80,74 @@ export default function AddFavourites() {
     return
   }
 
+  const handleChangeSelection = (field: string) => {
+    switch(field) {
+      case "to":
+        setTo('')
+        break
+
+      case "to":
+        setFrom('')
+        break
+    }
+    return setShowSearchInput(true);
+  }
+
+  const handleAddRate = () => {
+    return
+  }
+
+  const handleSearch = (input:string) => {
+    setSearchInput(input)
+
+    if (!input.trim()) {
+      return setSearchResults([...currencies.all])
+    }
+
+    let filteredResults = 
+      currencies.all
+      .filter(currency => currency
+        .toLowerCase()
+        .includes(
+          input
+          .toLocaleLowerCase()
+        ))
+    
+    return setSearchResults(filteredResults)
+  }
+
   return (
     <ScrollView className='p-4'>
       <View className='flex gap-4'>
-        <View className='flex gap-2'>
-          <Pressable className='p-4 rounded border border-neutral-300 bg-white'>
+        { showSearchInput && 
+          <View>
+            <TextInput 
+              onChangeText={input => handleSearch(input)}
+              placeholder='Type a currency'
+              value={searchInput}
+              style={styles.font_mono}
+              className='p-4 mb-4 rounded border border-neutral-300 bg-white placeholder:text-neutral-500' />
+            <FlatList 
+              data={searchResults}
+              renderItem={currency => 
+                <Pressable 
+                  onPress={() => handleCurrencyPress(currency.item)}
+                  key={currency.index}
+                  className='p-2 flex flex-row gap-4 hover:bg-neutral-300'>
+                  <Text style={styles.font_mono}>{currency.item}</Text>
+                  <Text 
+                    className='text-neutral-500'
+                    style={styles.font_mono}>
+                    {currencyName.of(currency.item)}
+                  </Text>
+                </Pressable>
+              }/>
+          </View>
+        }
+        <View className={`${showSearchInput && 'hidden'} flex gap-2`}>
+          <Pressable 
+            onPress={() => handleChangeSelection('from')}
+            className='p-4 rounded border border-neutral-300 bg-white'>
             { from ? 
               <View className='flex flex-row gap-2'>
                 <Text style={styles.font_mono}>{from}</Text>
@@ -98,7 +165,9 @@ export default function AddFavourites() {
               </Text>
             }
           </Pressable>
-          <Pressable className='p-4 rounded border border-neutral-300 bg-white'>
+          <Pressable 
+            onPress={() => handleChangeSelection('to')}
+            className='p-4 rounded border border-neutral-300 bg-white'>
             { to ? 
               <View className='flex flex-row gap-2'>
                 <Text style={styles.font_mono}>{to}</Text>
@@ -124,27 +193,28 @@ export default function AddFavourites() {
               size={styles.icon} />
           </Pressable>
         </View>
-        <View className={(from && to) && 'hidden'}>
+        <View className={`${showSearchInput && 'hidden'}`}>
           <Text 
             style={styles.font_mono}
-            className='mb-4 py-1 border-b border-neutral-300 text-neutral-700'>
+            className='mb-2 py-1 border-b border-neutral-300 text-neutral-700'>
             Suggested currencies
           </Text>
           {loadSuggestedCurrencies()}
           <Text 
             style={styles.font_mono}
-            className='my-4 py-1 border-b border-neutral-300 text-neutral-700'>
+            className='mt-4 mb-2 py-1 border-b border-neutral-300 text-neutral-700'>
             All currencies
           </Text>
           {loadAllCurrencies()}
+          <Pressable
+            onPress={handleAddRate}>
+            <Text 
+              style={styles.font_mono}
+              className='p-2 rounded text-center bg-blue-500 text-white'>
+              Add {from} to {to}
+            </Text>
+          </Pressable>
         </View>
-        <Pressable>
-          <Text 
-            style={styles.font_mono}
-            className='p-2 rounded text-center bg-blue-500 text-white'>
-            Add {from} to {to}
-          </Text>
-        </Pressable>
       </View>
     </ScrollView>
   );
