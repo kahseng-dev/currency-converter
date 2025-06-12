e5d7096d294b4a9905eed5322a4db40a92633802
kahseng-dev <kahseng.chew@protonmail.com>
2025-06-12 23:15:46 +0800

    feat: add toggle group component

---
+++ components/toggle-group.tsx
@@ -0,0 +1,33 @@
+import { Pressable, Text, View } from 'react-native';
+
+import { styles } from '@/constants/styles';
+
+export default function ToggleGroup({ 
+    options, 
+    setValue, 
+    value 
+  }: { 
+    options: string[], 
+    setValue: (value: string) => void, 
+    value: string 
+  }) {
+
+  const handleChangeOption = (option:string) => {
+    return setValue(option);
+  }
+
+  return (
+    <View className='p-1 flex flex-row bg-neutral-300 rounded-full'>
+      { options.map(option => 
+        <Pressable 
+          onPress={() => handleChangeOption(option)}
+          key={option}
+          className={`${(value == option) && 'bg-white'} p-2 w-1/3 rounded-full transition duration-300`}>
+          <Text 
+            style={styles.font_mono}
+            className='text-center'>{option}</Text>
+        </Pressable>
+      )}
+    </View>
+  )
+}
