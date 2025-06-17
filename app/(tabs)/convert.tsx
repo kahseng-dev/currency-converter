4b61f480d1840585c59933524bd9174e1320313a
kahseng-dev <kahseng.chew@protonmail.com>
2025-06-17 15:19:50 +0800

    feat: add convert page

--- app/(tabs)/convert.tsx
+++ app/(tabs)/convert.tsx
@@ -1,53 +1,83 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { router } from 'expo-router';
 import { useState } from 'react';
-import { Pressable, Text, View } from 'react-native';
+import { Pressable, Text, TextInput, View } from 'react-native';
 
 import { styles } from '@/constants/styles';
+import { Rate } from '@/types/rate';
 
 export default function Convert() {
   const [amount, setAmount] = useState(1000);
-  const [rate, setRate] = useState({base: 'USD', to: 'SGD', rate: 0});
+  const [rate, setRate] = useState<Rate>({ base: 'USD', to: 'SGD', rate: 0 });
+
+  const handleChangeCurrency = (field:string) => {
+    return router.push({ pathname: '/choose-currency', params: { field } });
+  }
+
+  const handleSwapFieldsPress = () => {
+    setRate(previous => ({
+      base: previous.to,
+      to: previous.base,
+      rate: previous.rate
+    }));
+  }
 
   return (
-    <View className='p-8 flex gap-8'>
-      <Pressable className='p-8 flex flex-row justify-between border-2 rounded-lg bg-white shadow'>
-        <View className='flex flex-row items-center gap-2'>
-          <Text
-            className='text-xl'
-            style={styles.font_mono}>
-            {rate.base}
-          </Text>
-          <Ionicons
-            name='chevron-down'
-            size={styles.icon} />
+    <View className='p-8 relative flex gap-4'>
+      <View className='flex gap-4'>
+        <View className='p-8 group flex flex-row justify-between border border-neutral-300 rounded-lg bg-white'>
+          <Pressable
+            onPress={() => handleChangeCurrency('base')}
+            className='flex flex-row items-center gap-2'>
+            <Text
+              className='text-xl'
+              style={styles.font_mono}>
+              {rate.base}
+            </Text>
+            <Ionicons
+              name='chevron-down'
+              size={styles.icon} />
+          </Pressable>
+          <TextInput
+            onChangeText={text => setAmount(parseFloat(text))}
+            className='text-xl text-right outline-none'
+            value={amount.toString()}
+            keyboardType='numeric'
+            style={styles.font_mono} />
         </View>
-        <Text
-          className='text-xl'
-          style={styles.font_mono}>
-          {amount}
-        </Text>
-      </Pressable>
-      <Pressable className='p-8 flex flex-row justify-between border-2 rounded-lg bg-white shadow'>
-        <View className='flex flex-row items-center gap-2'>
-          <Text
-            className='text-xl'
-            style={styles.font_mono}>
-            {rate.to}
-          </Text>
-          <Ionicons
-            name='chevron-down'
-            size={styles.icon} />
+        <View className='p-8 flex flex-row justify-between border border-neutral-300 rounded-lg bg-white'>
+          <Pressable 
+            onPress={() => handleChangeCurrency('to')}
+            className='flex flex-row items-center gap-2'>
+            <Text
+              className='text-xl'
+              style={styles.font_mono}>
+              {rate.to}
+            </Text>
+            <Ionicons
+              name='chevron-down'
+              size={styles.icon} />
+          </Pressable>
+          <TextInput
+            onChangeText={text => setAmount(parseFloat(text))}
+            className='text-xl text-right outline-none'
+            value={(amount * rate.rate).toString()}
+            keyboardType='numeric'
+            style={styles.font_mono} />
         </View>
-        <Text
-          className='text-xl'
-          style={styles.font_mono}>
-          {amount}
-        </Text>
-      </Pressable>
+        <Pressable 
+          onPress={handleSwapFieldsPress}
+          className='absolute right-8 top-0 bottom-0 m-auto size-8 flex items-center justify-center rounded-full bg-white border border-neutral-300'>
+          <Ionicons 
+            name='swap-vertical-outline'
+            size={styles.icon} />
+        </Pressable>
+      </View>
       <Text 
-        className='text-center'
+        className='py-2 text-center bg-neutral-300 rounded'
         style={styles.font_mono}>
-        1 {rate.base} = {rate.rate} {rate.to} at mid-market rate
+        1 {rate.base} = {rate.rate} {rate.to} 
+        <Text className='text-neutral-500'> at the mid-market rate</Text>
       </Text>
     </View>
   );
