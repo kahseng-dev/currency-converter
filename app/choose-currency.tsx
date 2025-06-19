a17c58436958610cb2fea5dc75d816161616e10e
kahseng-dev <kahseng.chew@protonmail.com>
2025-06-19 17:09:04 +0800

    fix: search and select currencies

--- app/choose-currency.tsx
+++ app/choose-currency.tsx
@@ -7,9 +7,8 @@ import { styles } from '@/constants/styles';
 
 export default function ChooseCurrency({ field }: { field: string }) {
 
-  const rate = { base: 'USD', to: 'SGD', rate: 0 } 
-  const previousCurrency = rate['base'];
-  
+  const rate = { base: 'USD', to: 'SGD', rate: 0 };
+
   const currencies = {
     suggested: ['USD','GBP', 'SGD', 'EUR', 'INR'],
     all: ['AED','ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CHF', 'CHF', 'CLP', 'CNH', 'CNY', 'COP', 'CRC', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ETB', 'EUR', 'FJD', 'FKP', 'GBP', 'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HTG', 'HUF', 'IDR', 'ILS', 'IMP', 'INR', 'ISK', 'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KMF', 'KRW', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MUR', 'MVR', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SEK', 'SGD', 'SHP', 'SLE', 'SLL', 'SRD', 'STD', 'SVC', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD', 'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'UYU', 'UZS', 'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XCG', 'XOF', 'XPF', 'ZAR', 'ZWG'],
@@ -17,11 +16,13 @@ export default function ChooseCurrency({ field }: { field: string }) {
 
   const [searchInput, setSearchInput] = useState('');
   const [searchResults, setSearchResults] = useState([...currencies.all]);
+  const [selectedCurrency, setSelectedCurrency] = useState(rate['base']);
 
   const currencyName = new Intl.DisplayNames(['en'], { type: 'currency' });
 
   const handleCurrencyPress = (currency: string) => {
-    return 
+    setSearchInput('');
+    return setSelectedCurrency(currency)
   }
 
   const loadSuggestedCurrencies = () => {
@@ -32,16 +33,11 @@ export default function ChooseCurrency({ field }: { field: string }) {
           <Pressable 
             onPress={() => handleCurrencyPress(currency.item)}
             key={currency.index}
-            className={`${currency.item === previousCurrency && 'border-2 border-neutral-300'} rounded p-2 flex flex-row items-center justify-between hover:bg-neutral-300`}>
-            <View className='flex flex-row gap-4'>
-              <Text style={styles.font_mono}>{currency.item}</Text>
-              <Text 
-                className='text-neutral-500'
-                style={styles.font_mono}>
-                {currencyName.of(currency.item)}
-              </Text>
-            </View>
-            { currency.item === previousCurrency &&
+            className={`${currency.item === selectedCurrency && 'bg-neutral-300'} p-2 flex flex-row items-center justify-between rounded hover:bg-neutral-400`}>
+            <Text style={styles.font_mono}>
+              {currency.item}  <Text className='text-neutral-500'>{currencyName.of(currency.item)}</Text>
+            </Text>
+            { currency.item === selectedCurrency &&
               <Ionicons 
                 name='checkmark-outline'
                 size={styles.icon} />
@@ -59,16 +55,13 @@ export default function ChooseCurrency({ field }: { field: string }) {
           <Pressable 
             onPress={() => handleCurrencyPress(currency.item)}
             key={currency.index}
-            className={`${currency.item === previousCurrency && 'border-2 border-neutral-300'} p-2 flex flex-row items-center justify-between rounded hover:bg-neutral-300`}>
+            className={`${currency.item === selectedCurrency && 'bg-neutral-300'} p-2 flex flex-row items-center justify-between rounded hover:bg-neutral-400`}>
             <View className='flex flex-row gap-4'>
-              <Text style={styles.font_mono}>{currency.item}</Text>
-              <Text 
-                className='text-neutral-500'
-                style={styles.font_mono}>
-                {currencyName.of(currency.item)}
+              <Text style={styles.font_mono}>
+                {currency.item}  <Text className='text-neutral-500'>{currencyName.of(currency.item)}</Text>
               </Text>
             </View>
-            { currency.item === previousCurrency &&
+            { currency.item === selectedCurrency &&
               <Ionicons 
                 name='checkmark-outline'
                 size={styles.icon} />
@@ -110,20 +103,42 @@ export default function ChooseCurrency({ field }: { field: string }) {
           style={styles.font_mono}
           className='w-full placeholder:text-neutral-500 outline-none' />
       </View>
-      <View>
-        <Text 
-          style={styles.font_mono}
-          className='mb-2 py-1 border-b border-neutral-300 text-neutral-700'>
-          Suggested currencies
-        </Text>
-        {loadSuggestedCurrencies()}
-        <Text 
-          style={styles.font_mono}
-          className='mt-4 mb-2 py-1 border-b border-neutral-300 text-neutral-700'>
-          All currencies
-        </Text>
-        {loadAllCurrencies()}
-      </View>
+      { searchInput != '' ?
+        <FlatList 
+          data={searchResults}
+          renderItem={ currency => 
+            <Pressable 
+              onPress={() => handleCurrencyPress(currency.item)}
+              key={currency.index}
+              className={`${currency.item === selectedCurrency && 'bg-neutral-300'} p-2 flex flex-row items-center justify-between rounded hover:bg-neutral-400`}>
+              <View className='flex flex-row gap-4'>
+                <Text style={styles.font_mono}>
+                  {currency.item}  <Text className='text-neutral-500'>{currencyName.of(currency.item)}</Text>
+                </Text>
+              </View>
+              { currency.item === selectedCurrency &&
+                <Ionicons 
+                  name='checkmark-outline'
+                  size={styles.icon} />
+              }
+            </Pressable>
+          }/>
+        :
+        <View>
+          <Text 
+            style={styles.font_mono}
+            className='mb-2 py-1 border-b border-neutral-300 text-neutral-700'>
+            Suggested currencies
+          </Text>
+          {loadSuggestedCurrencies()}
+          <Text 
+            style={styles.font_mono}
+            className='mt-4 mb-2 py-1 border-b border-neutral-300 text-neutral-700'>
+            All currencies
+          </Text>
+          {loadAllCurrencies()}
+        </View>
+      }
     </ScrollView>
   );
 }
