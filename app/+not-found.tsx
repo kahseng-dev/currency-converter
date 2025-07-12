cc6c747109507ed4ce10f18662524b5a4dafca98
kahseng-dev <kahseng.chew@protonmail.com>
2025-07-13 04:32:48 +0800

    perf: remove additional stack screen option

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,25 +1,22 @@
-import { Link, Stack } from 'expo-router';
+import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
 import { styles } from '@/constants/styles';
 
 export default function NotFound() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Not Found' }} />
-      <View className='flex-1 items-center justify-center'>
-        <Text 
-          style={styles.font_mono}
-          className='text-center mb-4'>
-          404: Page Not Found
-        </Text>
-        <Link 
-          href='./'
-          style={styles.font_mono}
-          className={styles.button} >
-          Go Home
-        </Link>
-      </View>
-    </>
+    <View className='flex-1 items-center justify-center'>
+      <Text 
+        style={styles.font_mono}
+        className='text-center mb-4'>
+        404: Page Not Found
+      </Text>
+      <Link 
+        href='./'
+        style={styles.font_mono}
+        className={styles.button}>
+        Go Home
+      </Link>
+    </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,8 @@ import '../styles/globals.css';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name='(tabs)' options={{headerShown: false}} />
-      <Stack.Screen name='+not-found' />
+      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen name='+not-found' options={{ title: 'Not Found' }} />
     </Stack>
   );
 }
