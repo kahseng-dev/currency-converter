0621db1e101c7e6e369f15b4290f9686c4062d89
kahseng-dev <kahseng.chew@protonmail.com>
2025-07-13 03:10:01 +0800

    feat: add props interface

--- components/segmented-controls.tsx
+++ components/segmented-controls.tsx
@@ -2,30 +2,25 @@ import { Pressable, Text, View } from 'react-native';
 
 import { styles } from '@/constants/styles';
 
-export default function SegmentedControls({ 
-    options, 
-    setValue, 
-    value 
-  }: { 
-    options: string[], 
-    setValue: (value: string) => void, 
-    value: string 
-  }) {
+interface SegmentedControlsProps { 
+  options: string[], 
+  setValue: (value: string) => void, 
+  value: string 
+}
 
-  const handleChangeOption = (option:string) => {
-    return setValue(option);
-  }
-
+export default function SegmentedControls({ options, setValue, value }: SegmentedControlsProps) {
   return (
     <View className='p-1 grid grid-flow-col gap-1 bg-neutral-300 rounded-full'>
       { options.map(option =>
         <Pressable 
-          onPress={() => handleChangeOption(option)}
+          onPress={() => setValue(option)}
           key={option}
-          className={`${(value == option) && 'bg-white'} p-2 rounded-full hover:bg-neutral-200 transition duration-300`}>
+          className={`${(value === option) && 'bg-white'} p-2 rounded-full hover:bg-neutral-200 transition duration-300`}>
           <Text 
             style={styles.font_mono}
-            className='text-center'>{option}</Text>
+            className='text-center'>
+            {option}
+          </Text>
         </Pressable>
       )}
     </View>
